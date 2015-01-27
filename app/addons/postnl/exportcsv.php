8bf3ad316c660dd4f826cdaecbe664ee3b30c102
Radu <radu.kovaci@busyweb.nl>
2015-01-27 20:38:00 +0200

    update post.nl addon

--- app/addons/postnl/exportcsv.php
+++ app/addons/postnl/exportcsv.php
@@ -1,7 +1,14 @@
 <?php
- 
-function convert_to_csv($input_array, $output_file_name, $delimiter)
-{
+
+$array_to_csv = array(
+	array('Naam 1','Naam 2','Naam 3','Straatnaam','Huisnummer','Huisnummer toevoeging','Postcode','Woonplaats','Landsdeel','Landcode','naampakbonbestand','opmerking','volgnummer'),
+	array('AC Heijer','','2313600_37663','Burgemeester Keijzerlaan','185','','2262BG','Leidschendam','','Nederland','C:\UnderfashionDatabase\pakbonnen\2313600_37663PakbonEnPicklijst.pdf','','1'),
+	array('Francoise Tai','','2313600_37736','Bergdreef','54','','4822TM','Breda','','Nederland','C:\UnderfashionDatabase\pakbonnen\2313600_37736PakbonEnPicklijst.pdf','','2'),
+	array('mja jollie','','2313600_37735','Beelaertslaan','21','','6861AS','Oosterbeek','','Nederland','C:\UnderfashionDatabase\pakbonnen\2313600_37735PakbonEnPicklijst.pdf','','3'),
+	array('Frederieke Linckens','','2313600_37560','Uitdamstraat','19','','3826CG','Amersfoort','','Nederland','C:\UnderfashionDatabase\pakbonnen\2313600_37560(3)PakbonEnPicklijst.pdf','+pakketzegel voor retourneren verkeerde bh op onze kosten','4')
+);
+
+function convert_to_csv($input_array, $output_file_name, $delimiter) {
     /** open raw memory as file, no need for temp files */
     $temp_memory = fopen('php://memory', 'w');
     /** loop through array  */
@@ -17,24 +24,7 @@ function convert_to_csv($input_array, $output_file_name, $delimiter)
     /** Send file to browser for download */
     fpassthru($temp_memory);
 }
- 
-/** Array to convert to csv */
- 
-$array_to_csv = Array(
-    Array(12566,
-        'Enmanuel',
-        'Corvo'
-    ),
-    Array(56544,
-        'John',
-        'Doe'
-    ),
-    Array(78550,
-        'Mark',
-        'Smith'
-    )
- 
-);
+
  
 convert_to_csv($array_to_csv, 'report.csv', ',');
 ?>
